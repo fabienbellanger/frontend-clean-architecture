f0cb0797402a26d78688d7258fc047112de5d112
Fabien Bellanger <fabien.bellanger@apitic.com>
2023-03-03 16:20:27 +0100

    Add Pinia store

--- packages/infrastructure/vuejs/src/views/HomeView.vue
+++ packages/infrastructure/vuejs/src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <h1>Counter store</h1>
+        <div><b>{{store.count}}</b></div>
+        <button v-on:click="store.increment()">Increment counter</button>
+    </main>
 </template>
+
+<script lang="ts" setup>
+import { useCounterStore } from '@/stores/counter';
+
+const store = useCounterStore();
+</script>
